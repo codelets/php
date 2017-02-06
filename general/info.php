6adc89768f27aa628f387a4ec916561d8a6ec9dc
codelets <bnrghvn@gmail.com>
2017-02-06 13:59:14 +0530

    Update info.php

--- general/info.php
+++ general/info.php
@@ -1,6 +1,6 @@
 <?php 
-    phpinfo(INFO_GENERAL);
-    phpinfo(INFO_CONFIGURATION);
-    phpinfo(INFO_MODULES);
-    phpinfo(INFO_ENVIRONMENT);
-    phpinfo(INFO_VARIABLES);
+    phpinfo(INFO_GENERAL); # information about php ini files, installation locations etc
+    phpinfo(INFO_CONFIGURATION); # information about PHP directives
+    phpinfo(INFO_MODULES); # modules loaded and module configurations
+    phpinfo(INFO_ENVIRONMENT); #enviornment settings affecting the PHP installation
+    phpinfo(INFO_VARIABLES); # information about the predefined variables
